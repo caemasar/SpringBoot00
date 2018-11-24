ede93964522dedd20723bcdd413df367a07a96df
Caemasar <caemasar@protonmail.com>
2018-11-25 01:47:50 +0800

    通过三个App*Package.java验证可知，SpringBootApplication注解会对子包类进行扫描，但是同级不行，父级不行。

--- src/main/java/idv/caemasar/demo/AppSuperiorPackage.java
+++ src/main/java/idv/caemasar/demo/AppSuperiorPackage.java
@@ -5,7 +5,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class AppSuperiorPackage {
-	public static void main(String[] args) {
-		SpringApplication.run(AppSuperiorPackage.class, args);
-	}
+    /* 这里证明SpringBootApplication注解会对其下层类进行扫描，即如果是SameLevel则扫描不到其他同级包
+     */
+
+    public static void main(String[] args) {
+        SpringApplication.run(AppSuperiorPackage.class, args);
+    }
 }
